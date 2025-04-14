c7618a549ace6307465dd24d6a5a18ff85abac93
nobrainzz <keel@artificiel.ch>
2025-04-14 16:38:15 +0200

    change config

--- medusa/medusa-config.js
+++ medusa/medusa-config.js
@@ -19,7 +19,7 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || 'supersecret',
       cookieSecret: process.env.COOKIE_SECRET || 'supersecret',
     },
-    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
+    workerMode: process.env.MEDUSA_WORKER_MODE,
     redisUrl: process.env.REDIS_URL,
   },
   modules: [
