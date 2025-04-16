486f9ef076e51cbe59accf5a255123c43d49048c
keel <82108616+keelchan@users.noreply.github.com>
2025-04-16 09:37:58 +0200

    Update medusa-config.js

--- medusa/medusa-config.js
+++ medusa/medusa-config.js
@@ -188,5 +188,25 @@ module.exports = defineConfig({
         },
       },
     },
+    {
+      resolve: "@medusajs/medusa/cache-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/event-bus-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/workflow-engine-redis",
+      options: {
+        redis: {
+          url: process.env.REDIS_URL,
+        },
+      },
+    },
   ],
 })
